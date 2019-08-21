57e021dc78bc007fc04fbd75d817c0b997c4b3f0
Milind Dhoke <mdhoke@qualys.com>
2019-08-21 00:05:57 -0700

    adding function to sort alphabets

--- algorithms/bubblesort.go
+++ algorithms/bubblesort.go
@@ -21,7 +21,8 @@ import (
 )
 
 var (
-	dataset = []int{406, 140, 727, 96, 719, 563, 132, 510, 698, 196}
+	dataset   = []int{406, 140, 727, 96, 719, 563, 132, 510, 698, 196}
+	alphabets = []rune{'A', 'F', 'K', 'E', 'B', 'Q', 'M', 'P', 'S', 'C', 'D', 'G'}
 )
 
 func bubblesort(data []int) {
@@ -41,10 +42,33 @@ func bubblesort(data []int) {
 	fmt.Println("Total Swaps -->", swap)
 	fmt.Println("Total pass -->", pass)
 }
+
+func sortAlphabets(alpha []rune) {
+	pass := 0
+	swap := 0
+	for i := 0; i < len(alpha); i++ {
+		for j := 0; j < len(alpha)-1; j++ {
+			if alpha[j] > alpha[j+1] {
+				alpha[j], alpha[j+1] = alpha[j+1], alpha[j]
+				swap++
+			} else {
+				pass++
+			}
+
+		}
+	}
+	fmt.Println("Total Swaps -->", swap)
+	fmt.Println("Total pass -->", pass)
+}
+
 func main() {
 	fmt.Println("Original unsorted data is -->", dataset)
 	fmt.Println("-----------------------------------------")
 	bubblesort(dataset)
 	fmt.Println("-----------------------------------------")
 	fmt.Println("Sorted data set is -->", dataset)
+	fmt.Println("-----------------------------------------")
+	fmt.Println("Original unsorted alphabets are -->", string(alphabets))
+	sortAlphabets(alphabets)
+	fmt.Println("Sorted alphabets are -->", string(alphabets))
 }
